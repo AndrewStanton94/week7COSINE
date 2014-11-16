ca531156c18d94f3ca167d949516135fede9e724
AndrewStanton94 <up683693@myport.ac.uk>
2014-11-16 22:45:47 +0000

    Annotations and .gitignore

--- MyClient.java
+++ MyClient.java
@@ -2,9 +2,11 @@ import java.rmi.registry.*;
 
 public class MyClient{
 	public static void main(String args []) throws Exception{
-		Registry reg = LocateRegistry.getRegistry("148.197.144.182", 1234);
+		Registry reg = LocateRegistry.getRegistry("148.197.53.207", 1234);	// Uses socket: IP + port to retreive remote object reference from server
+																				// https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html#getRegistry(java.lang.String,%20int)
 		MyRemoteInterface handle =
-			(MyRemoteInterface) reg.lookup("myrmiserver");
-		handle.printMessage("Wassup!");
+			(MyRemoteInterface) reg.lookup("myrmiserver");	// Return object linked to key. Cast to MyRemoteInterface.
+																// https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html#getRegistry(java.lang.String,%20int)
+		handle.printMessage("Wassup!");		// Execute remote method.
 	}
 }
--- MyServer.java
+++ MyServer.java
@@ -12,8 +12,11 @@ public class MyServer extends UnicastRemoteObject
 	public static void main(String args []) throws Exception {
 		MyServer server = new MyServer();
 
-		Registry reg = LocateRegistry.createRegistry(1234);
-		reg.bind("myrmiserver", server);
+		Registry reg = LocateRegistry.createRegistry(1234);	// Store remote object in local registry, Bypasses process separation.
+															//	Listen on port 1234
+																//	https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html
+		reg.bind("myrmiserver", server);	// Key, value tuple to be stored in registry
+												//https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/Registry.html#bind(java.lang.String,%20java.rmi.Remote)
 	}
 
 	public MyServer() throws RemoteException {}
