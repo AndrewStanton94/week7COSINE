0a23ab663569867c8810540c999b427f71a64eee
AndrewStanton94 <up683693@myport.ac.uk>
2014-11-21 17:47:10 +0000

    Interactive socket data acquisition

--- MyClient.java
+++ MyClient.java
@@ -1,15 +1,16 @@
 import java.rmi.registry.*;
+import java.rmi.ConnectException;
 import java.io.*;
 
 public class MyClient{
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void main(String args []) throws Exception{
-		Registry reg = LocateRegistry.getRegistry("148.197.53.207", 1234);	// Uses socket: IP + port to retreive remote object reference from server
-																				// https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html#getRegistry(java.lang.String,%20int)
+		Registry reg = locateRegistry();
 		MyRemoteInterface handle =
 			(MyRemoteInterface) reg.lookup("myrmiserver");	// Return object linked to key. Cast to MyRemoteInterface.
 																// https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html#getRegistry(java.lang.String,%20int)
 		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 			boolean more = true;
 			while (more){
 				String message = in.readLine();
@@ -21,5 +22,15 @@ public class MyClient{
 		catch (Exception ex){
 			System.err.println(ex);
 		}
+	}
+
+	private static Registry locateRegistry() throws Exception{
+		System.out.println("Enter the IP address of the sever: ");
+		String ip = in.readLine();
+		System.out.println("Enter port: ");
+		int port = Integer.parseInt(in.readLine());
+		return LocateRegistry.getRegistry(ip, port);
+			// Uses socket: IP + port to retreive remote object reference from server
+				// https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html#getRegistry(java.lang.String,%20int)
 	}
 }
--- MyServer.java
+++ MyServer.java
@@ -1,9 +1,15 @@
 import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*;
+import java.net.InetAddress;
+import java.io.*;
 
 public class MyServer extends UnicastRemoteObject
 	implements MyRemoteInterface {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public MyServer() throws RemoteException {}
 
 	public void printMessage(String message) throws RemoteException {
 		System.out.println(message);
@@ -12,12 +18,19 @@ public class MyServer extends UnicastRemoteObject
 	public static void main(String args []) throws Exception {
 		MyServer server = new MyServer();
 
-		Registry reg = LocateRegistry.createRegistry(1234);	// Store remote object in local registry, Bypasses process separation.
-															//	Listen on port 1234
-																//	https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html
+		Registry reg = createRegistry();
 		reg.bind("myrmiserver", server);	// Key, value tuple to be stored in registry
 												//https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/Registry.html#bind(java.lang.String,%20java.rmi.Remote)
+
+		System.out.println("Server established");
+		System.out.println(InetAddress.getLocalHost());		// http://www.programmingsimplified.com/java/source-code/java-program-ip-address
 	}
 
-	public MyServer() throws RemoteException {}
+	private static Registry createRegistry() throws Exception{
+		System.out.println("Enter port: ");
+		int port = Integer.parseInt(in.readLine());
+		return LocateRegistry.createRegistry(port);	// Store remote object in local registry, Bypasses process separation. Listen on port 1234
+														//	https://docs.oracle.com/javase/7/docs/api/java/rmi/registry/LocateRegistry.html
+		
+	}
 }
